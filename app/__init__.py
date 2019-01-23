a35b1156e1ac1ace586a65970f0407486da37890
TauCrus <TauCrus@163.com>
2019-01-23 18:30:09 +0800

    day 2

---
+++ app/__init__.py
@@ -0,0 +1,16 @@
+import logging
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from config import Config
+
+db = SQLAlchemy()
+
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+
+    # 添加插件
+    db.init_app(app)
+
+    return app
---
+++ config.py
@@ -0,0 +1,16 @@
+import os
+from dotenv import load_dotenv
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
+
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    # 数据库
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # 每页展示的数据列表长度
+    POSTS_PER_PAGE = 20
