4f3950d96d581c01ebc42384149388d559f28122
TauCrus <TauCrus@163.com>
2019-01-24 18:31:17 +0800

    day 3

---
+++ .flaskenv
@@ -0,0 +1,1 @@
+Flask_APP=admin_back.py
--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,23 @@
-import logging
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 db = SQLAlchemy()
+migrate = Migrate()
 
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
 
-    # 添加插件
     db.init_app(app)
+    migrate.init_app(app, db)
+
+    from app.api import bp as api_bp
+    app.register_blueprint(api_bp, url_prefix='/api')
 
     return app
+
+
+from app import models
